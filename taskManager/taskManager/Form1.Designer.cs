9841277e28296b022dee9d613f5e2f36ef2e4049
Bilal Öztemiz <44530060+bilaloztmz@users.noreply.github.com>
2021-08-15 13:32:38 +0300

    dosyalari yukledim

---
+++ taskManager/taskManager/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace taskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.proseslerlistesi = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.threadSayısıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kullandığıBellekMiktarıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.öldürToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.secilenProses = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.uyarı = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // proseslerlistesi
+            // 
+            this.proseslerlistesi.ContextMenuStrip = this.contextMenuStrip1;
+            this.proseslerlistesi.FormattingEnabled = true;
+            this.proseslerlistesi.ItemHeight = 16;
+            this.proseslerlistesi.Location = new System.Drawing.Point(30, 48);
+            this.proseslerlistesi.Name = "proseslerlistesi";
+            this.proseslerlistesi.Size = new System.Drawing.Size(430, 404);
+            this.proseslerlistesi.TabIndex = 0;
+            this.proseslerlistesi.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.threadSayısıToolStripMenuItem,
+            this.kullandığıBellekMiktarıToolStripMenuItem,
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem,
+            this.öldürToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(250, 100);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // threadSayısıToolStripMenuItem
+            // 
+            this.threadSayısıToolStripMenuItem.Name = "threadSayısıToolStripMenuItem";
+            this.threadSayısıToolStripMenuItem.Size = new System.Drawing.Size(249, 24);
+            this.threadSayısıToolStripMenuItem.Text = "Thread Sayısı";
+            this.threadSayısıToolStripMenuItem.Click += new System.EventHandler(this.threadSayısıToolStripMenuItem_Click);
+            // 
+            // kullandığıBellekMiktarıToolStripMenuItem
+            // 
+            this.kullandığıBellekMiktarıToolStripMenuItem.Name = "kullandığıBellekMiktarıToolStripMenuItem";
+            this.kullandığıBellekMiktarıToolStripMenuItem.Size = new System.Drawing.Size(249, 24);
+            this.kullandığıBellekMiktarıToolStripMenuItem.Text = "Kullandığı Bellek Miktarı";
+            this.kullandığıBellekMiktarıToolStripMenuItem.Click += new System.EventHandler(this.kullandığıBellekMiktarıToolStripMenuItem_Click);
+            // 
+            // kullandığıİşlemciYüzdesiToolStripMenuItem
+            // 
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem.Name = "kullandığıİşlemciYüzdesiToolStripMenuItem";
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem.Size = new System.Drawing.Size(249, 24);
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem.Text = "Kullandığı İşlemci Yüzdesi";
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem.Click += new System.EventHandler(this.kullandığıİşlemciYüzdesiToolStripMenuItem_Click);
+            // 
+            // öldürToolStripMenuItem
+            // 
+            this.öldürToolStripMenuItem.Name = "öldürToolStripMenuItem";
+            this.öldürToolStripMenuItem.Size = new System.Drawing.Size(249, 24);
+            this.öldürToolStripMenuItem.Text = "Öldür";
+            this.öldürToolStripMenuItem.Click += new System.EventHandler(this.öldürToolStripMenuItem_Click);
+            // 
+            // secilenProses
+            // 
+            this.secilenProses.Location = new System.Drawing.Point(168, 493);
+            this.secilenProses.Multiline = true;
+            this.secilenProses.Name = "secilenProses";
+            this.secilenProses.Size = new System.Drawing.Size(186, 22);
+            this.secilenProses.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 493);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seçilen Proses";
+            // 
+            // uyarı
+            // 
+            this.uyarı.AutoSize = true;
+            this.uyarı.Location = new System.Drawing.Point(205, 18);
+            this.uyarı.Name = "uyarı";
+            this.uyarı.Size = new System.Drawing.Size(221, 17);
+            this.uyarı.TabIndex = 4;
+            this.uyarı.Text = "CPU KULLANIMI HESAPLANIYOR";
+            this.uyarı.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 559);
+            this.Controls.Add(this.uyarı);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.secilenProses);
+            this.Controls.Add(this.proseslerlistesi);
+            this.Name = "Form1";
+            this.Text = "Görev Yöneticisi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox proseslerlistesi;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem threadSayısıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kullandığıBellekMiktarıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kullandığıİşlemciYüzdesiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem öldürToolStripMenuItem;
+        private System.Windows.Forms.TextBox secilenProses;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label uyarı;
+    }
+}
+
---
+++ taskManager/taskManager/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taskManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Process[] prosesler;
+
+        void prosesleriListele()
+        {
+            prosesler = Process.GetProcesses();
+            proseslerlistesi.Items.Clear();
+            foreach (Process p in prosesler)
+            {
+                proseslerlistesi.Items.Add(p.ProcessName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            secilenProses.Enabled = false;
+            prosesleriListele();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void öldürToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if(proseslerlistesi.SelectedIndex < 0)
+            {
+                MessageBox.Show("Proses seçiniz");
+            }
+            else
+            {
+                try
+                {
+                    string silinecek = prosesler[proseslerlistesi.SelectedIndex].ProcessName;
+                    prosesler[proseslerlistesi.SelectedIndex].Kill();
+                    MessageBox.Show(silinecek + "  öldürüldü");
+                    prosesleriListele();
+
+                }
+                catch (Exception hata)
+                {
+                    MessageBox.Show(hata.Message);
+                    prosesleriListele();
+                }
+            }
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            secilenProses.Text = proseslerlistesi.SelectedItem.ToString();
+
+        }
+
+        private void threadSayısıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show(prosesler[proseslerlistesi.SelectedIndex].ProcessName + " Thread sayısı: " + prosesler[proseslerlistesi.SelectedIndex].Threads.Count.ToString());
+                prosesleriListele();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Proses seçiniz.");
+            }
+        
+        }
+
+        private void kullandığıBellekMiktarıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double toplamByte = prosesler[proseslerlistesi.SelectedIndex].PagedMemorySize64 / 1048576;
+                MessageBox.Show(prosesler[proseslerlistesi.SelectedIndex].ProcessName + " Kullandığı bellek miktarı:  " + toplamByte.ToString() + "MB");
+                //1 mb = 1.048.576 byte
+                prosesleriListele();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Proses seçiniz.");
+            }
+
+
+
+        }
+
+        private void kullandığıİşlemciYüzdesiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            uyarı.Visible = true;
+            try
+            {
+                PerformanceCounter işlemcideProses = new PerformanceCounter("Process", "% Processor Time", prosesler[proseslerlistesi.SelectedIndex].ProcessName, true);
+                double işlemciYüzdesi = işlemcideProses.NextValue();
+                Thread.Sleep(1500);
+                işlemciYüzdesi = işlemcideProses.NextValue();
+                uyarı.Visible = false;
+                MessageBox.Show(prosesler[proseslerlistesi.SelectedIndex].ProcessName + " Kullandığı işlemci yüzdesi %" + işlemciYüzdesi.ToString());
+                prosesleriListele();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Proses seçiniz.");
+
+            }
+
+
+
+
+
+        }
+    }
+}
